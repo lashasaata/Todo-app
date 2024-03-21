cd2f98ef3c2307f6dade657b31e8bee20dd17db2
lashasaata <lashasaata@gmail.com>
2024-03-21 12:28:12 -0700

    add modification in other fails

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-
+import Creator from "./comtonents/Creator";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return;
+  return (
+    <div className="flex flex-col items-center min-h-screen relative">
+      <div className="h-[200px] min-w-full bg-[url('./images/bg-mobile-light.jpg')] bg-no-repeat bg-cover"></div>
+      <Creator />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    boxShadow: {
+      creatorShadow: "0 35px 50px -15px rgba(194, 195, 214, 0.5)",
+    },
   },
   plugins: [],
 };
