c95691df526fc9fe4724056f5045a17e606c9b36
lashasaata <lashasaata@gmail.com>
2024-03-22 09:24:28 -0700

    replace chackbox to div and have styled itcreate handlers and give them functional to add todoes in list

--- src/comtonents/Creator.jsx
+++ src/comtonents/Creator.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import "./Creator.css";
-function Creator() {
+
+function Creator(props) {
+  const [useButton, setUseButton] = useState(false);
+  const compliteHandler = () => {
+    setUseButton(!useButton);
+  };
+  const [useInput, setUseInput] = useState();
+  const inputHAndler = (e) => {
+    setUseInput(e.target.value);
+  };
+  const handleEnter = (e) => {
+    let index = Math.floor(Math.random() * 10000);
+    if (e.key === `Enter`) {
+      const newToDO = {
+        complited: useButton,
+        toDo: useInput,
+        id: index,
+      };
+      props.setUseToDo([...props.useToDo, newToDO]);
+      console.log(props.useToDo);
+      console.log(index);
+    }
+  };
   return (
-    <div className="absolute mt-12">
+    <div className="mt-10">
       <div className="flex items-center justify-between">
         <h1 className="text-white text-[20px] font-[700] tracking-[14px]">
           TODO
@@ -13,14 +36,26 @@ function Creator() {
         />
       </div>
       <div className="w-[327px] h-[48px] rounded-[5px] bg-white pl-[20px] flex items-center gap-[12px] mt-10 shadow-creatorShadow">
+        <button
+          onClick={compliteHandler}
+          className={`${
+            useButton
+              ? "bg-gradient-to-br from-[#5df] to-[#c058f3]"
+              : "bg-white border border-solid border-slate-200"
+          } w-5 h-5 rounded-full flex justify-center items-center`}
+        >
+          {useButton ? (
+            <img src="./images/icon-check.svg" alt="check_icon" />
+          ) : (
+            ""
+          )}
+        </button>
         <input
-          type="checkBox"
-          className="appearance-none w-5 h-5 rounded-full border border-solid border-slate-200 checked:bg-[url('./images/icon-check')] checked:bg-gradient-to-br from-[#5df] to-[#c058f3]"
-        />
-        <input
+          onChange={inputHAndler}
+          onKeyDown={handleEnter}
           type="text"
           placeholder="Create a new todo…"
-          className="outline-none placeholder:text-xs placeholder:text-[#9495a5] placeholder:font-[500] placeholder:leading-[-0.17px] mt-[-4px]"
+          className="outline-none text-xs text-[#393a4b] font-[500] tracking-[-0.25px] w-[250px] placeholder:text-xs placeholder:text-[#9495a5] placeholder:font-[500] placeholder:leading-[-0.17px] "
         />
       </div>
     </div>
